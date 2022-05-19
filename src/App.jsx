72cbcfaeacb8d30c644b1d7d0c49978b1b7f9421
jhony75 <jonatas.lima.medeiros@live.com>
2022-05-19 01:45:15 -0300

    fix(api): axios instance initialized.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './styles/global.scss';
 
 export default function App() {
-  return <h1>Header</h1>;
+  return <h1>Inside Query Client</h1>;
 }
---
+++ src/api/fieloRequest.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const baseURL = 'https://challenge-fielo.herokuapp.com';
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQ2hhbGxlbmdlIEV1cmVrYSIsImRlc2NyaXB0aW9uIjoiRGVzYWZpbyBkZSBmcm9udC1lbmQiLCJjb21wYW55IjoiRmllbG8iLCJpYXQiOjE2NTI5MzA1ODEsImV4cCI6MTY1MjkzNDE4MX0.3C0Sp-82z9R-3aIJOxcR9ztRRT5hGvu_iAGhq7BwDE0';
+
+axios.get();
