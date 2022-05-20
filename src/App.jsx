2b4035b0937dfb671b3fc0bf16dc837f944f8a24
jhony75 <jonatas.lima.medeiros@live.com>
2022-05-19 23:50:21 -0300

    feat(api): requests completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import {
+  Auth,
+  Users,
+  UserById,
+  UserActivities,
+  Programs,
+  ProgramsLevels,
+  Levels,
+} from './api/fieloRequest';
+
 import './styles/global.scss';
 
 export default function App() {
-  return <h1>Inside Query Client</h1>;
+  return (
+    <>
+      <h1>Inside Query Client</h1>
+      <Auth />
+      <Users />
+      <UserById />
+      <UserActivities />
+      <Programs />
+      <ProgramsLevels />
+      <Levels />
+    </>
+  );
 }
