43663d74ebef7d9a4888447cf9adcb8e17231e21
jhony75 <jonatas.lima.medeiros@live.com>
2022-05-23 23:56:40 -0300

    chore(app): updated for tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Card from './components/card/Card';
 import './styles/global.scss';
 import styles from './app.module.scss';
+import { UsersList } from './components/usersList/UsersList';
 
 export default function App() {
   return (
     <>
       <h1>Inside Query Client</h1>
       <div className={styles.cardsContainer}>
-        <Card content="card" />
+        <UsersList />
         <Card content="card" />
         <Card content="card" />
       </div>
