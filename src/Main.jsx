c656f198fd66d01006fe9b65e460c3d71b71afc9
jhony75 <jonatas.lima.medeiros@live.com>
2022-05-27 23:03:59 -0300

    feat(Main): now it calls the newly created cards

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,6 @@
-import { Card } from './components/card/Card';
 import { UsersList } from './components/usersList/UsersList';
+import { UserData } from './components/userData/UserData';
+import { UserFeed } from './components/userFeed/UserFeed';
 
 import styles from './styles/main.module.scss';
 
@@ -9,8 +10,8 @@ const Main = () => {
       <h1>New Roots</h1>
       <div className={styles.cardsContainer}>
         <UsersList />
-        <Card content="card" />
-        <Card content="card" />
+        <UserData />
+        <UserFeed />
       </div>
     </>
   );
