7fc3176bfeb9d2baa82d758ccde0c79c8ebf417b
jhony75 <jonatas.lima.medeiros@live.com>
2022-05-27 22:23:55 -0300

    feat(app): now react runs on strict mode)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from 'react/cjs/react.production.min';
 import { Card } from './components/card/Card';
 import { UsersList } from './components/usersList/UsersList';
+import { StrictMode } from 'react';
 
 import styles from './styles/app.module.scss';
 
 export default function App() {
-  useEffect;
-
   return (
     <>
-      <h1>New Roots</h1>
-      <div className={styles.cardsContainer}>
-        <UsersList />
-        <Card content="card" />
-        <Card content="card" />
-      </div>
+      <StrictMode>
+        <h1>New Roots</h1>
+        <div className={styles.cardsContainer}>
+          <UsersList />
+          <Card content="card" />
+          <Card content="card" />
+        </div>
+      </StrictMode>
     </>
   );
 }
